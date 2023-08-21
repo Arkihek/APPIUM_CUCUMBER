2a2990d866631aeacaedd9b5dd7090ab109a429d
ihsan YILMAZ <124254928+Arkihek@users.noreply.github.com>
2023-08-21 17:55:33 +0300

    Day01

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -11,6 +11,15 @@ import org.junit.runner.RunWith;
         glue = {"stepdefinitions"},
         tags = "@deneme",
         dryRun = false
+        /*
+        Eger ki featureslarda bizim eksik yani daha once tanimlanmamis (sari sekilde gozukmesi) scenriolari belirlemek icin
+        eksik adim olup olmadigini dryRun`i TRUE yaparak kontrol etmis oluruz.
+
+        Eger ki testlerimiz passed olursa bu testlerimizin basarili oldugunu degil eksik adimlarin olmadigi
+        yani testlerimizin kosturmaya hazir oldugu anlamina gelmektedir.
+
+        Eger ki eksik adimimiz yok iste tum islemeleri tamamladigimizda TESTLERIMI KOSTURMAK ICIN DRYRUN`i FALSE yaparim.
+         */
 )
 
 public class Runner {
